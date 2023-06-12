9ccfdee03b244c48aebeb6c70381e23c0cff9fe6
Daniel Arenas <danielarenas@lahaus.com>
2023-06-11 21:04:06 -0500

    add  session details to README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 
+## LOGIN
+
+http://localhost:3002/users/sign_in
+
+User: admin@recipe.com
+pass: 123456789
+
+## ADMIN recipes
+
+http://localhost:3002/recettes_admin
 
 ##  Up database
 docker-compose -f docker-compose.yml up -d
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  mount Motor::Admin => '/motor_admin'
+  mount Motor::Admin => '/recettes_admin'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
