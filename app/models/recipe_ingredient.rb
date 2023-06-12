44182064c1fce0eaed2d7f638d7d29ea367e3bea
Daniel Arenas <danielarenas@lahaus.com>
2023-06-11 21:34:36 -0500

    Add validation to recipe ingredient

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,6 @@
 class RecipeIngredient < ApplicationRecord
   belongs_to :recipe
   belongs_to :ingredient
+
+  validates :recipe, uniqueness: { scope: :ingredient, message: "relation already created" }
 end
